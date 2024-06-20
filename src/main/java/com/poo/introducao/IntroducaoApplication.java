dae6328879ae3ab85c1769c65e434f1638efb892
Lucas Carvalho <blujack11@gmail.com>
2024-06-20 13:43:27 -0300

    Envio do exercício 07 da Lista03

--- src/main/java/com/poo/introducao/IntroducaoApplication.java
+++ src/main/java/com/poo/introducao/IntroducaoApplication.java
@@ -10,7 +10,7 @@ public class IntroducaoApplication {
         SpringApplication.run(IntroducaoApplication.class, args);
 
         Lista03 lista = new Lista03();
-        lista.ex6();
+        lista.ex7();
 		
 }
 }
--- src/main/java/com/poo/introducao/Lista03.java
+++ src/main/java/com/poo/introducao/Lista03.java
@@ -163,24 +163,27 @@ while (true) {
     }sc.close();
 
     }
-    
 
+    static void ex7(){
+    Scanner sc = new Scanner(System.in);
 
+    System.out.print("Digite a largura do retângulo: ");
+    int largura = sc.nextInt();
 
+    System.out.print("Digite a altura do retângulo: ");
+    int altura = sc.nextInt();
 
-
-
-
-
-
-
-
-
-
-
-
+    for (int i = 1; i <= altura; i++) {
+        
+    for (int j = 1; j <= largura; j++) {
+    System.out.print("*");
+    }      
+    System.out.println();
+    } sc.close();
 
     }
+
+}
 
 
 
